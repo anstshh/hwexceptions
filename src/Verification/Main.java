0829fd14565f1523fc6e7840a9e48f4b4f6b7e60
Admin <anstdor@gmail.com>
2022-11-05 13:17:25 +0300

    Exceptions Part 1 is done++

--- src/Verification/Main.java
+++ src/Verification/Main.java
@@ -2,7 +2,7 @@ package Verification;
 
 public class Main {
     public static void main(String[] args) {
-        boolean success = Data.validate("test","test","tаest");
+        boolean success = Data.validate("test","test","test");
         if(success){
             System.out.println("Данные верны!");
         }else{
